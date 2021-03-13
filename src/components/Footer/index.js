b078ecfc86f8e90e6fa1daa38a38d7f73bd9a6ed
eramos3 <cheddarshreddar3@gmail.com>
2021-03-13 12:56:00 -0800

    added readme

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
   return (
     <div>
       <footer>
-        <a alt="linkedin" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/eder-r-224074127"><FontAwesomeIcon icon={faLinkedin} size="3x"/></a>
-        <a alt="github" target="_blank" rel='noreferrer' href="https://github.com/eramos3"><FontAwesomeIcon icon={faGithub} size="3x"/></a>
+        <a alt="linkedin" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/eder-r-224074127"><FontAwesomeIcon icon={faLinkedin} size="3x" /></a>
+        <a alt="github" target="_blank" rel='noreferrer' href="https://github.com/eramos3"><FontAwesomeIcon icon={faGithub} size="3x" /></a>
+        <a alt="stackoverflow" target="_blank" rel='noreferrer' href="https://stackexchange.com/users/19668447/eramos"><FontAwesomeIcon icon={faStackOverflow} size="3x" /></a>
       </footer>
     </div>
   );
