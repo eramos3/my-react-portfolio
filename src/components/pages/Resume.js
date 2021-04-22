675c3a2da6f42ebe31e8549ba94c9f84ee8373a0
eramos3 <cheddarshreddar3@gmail.com>
2021-04-21 18:12:34 -0700

    link updated

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import resume from '../../assets/Eder F Ramos.pdf'
 const Resume = () => (
   <div>
     <h1>Resume</h1>
     <button className="w3-button w3-light-grey w3-padding-large w3-margin-top">
-      <a href="./assets/images/fcbarca.jpg" download="Eder's Resume"><FontAwesomeIcon icon={faDownload} size="3x"/> Download Resume</a>
+      <a href= {resume} download="Eder's Resume"><FontAwesomeIcon icon={faDownload} size="3x"/> Download Resume</a>
     </button>
-
-    <p>Will Add Resume once Completed</p>
   </div>
 );
 
