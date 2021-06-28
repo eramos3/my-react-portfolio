b1daafeb4b07869abe902a74c82df8fae70ea807
eramos3 <cheddarshreddar3@gmail.com>
2021-06-27 20:26:30 -0700

    fix contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,62 +1,16 @@
-import React,{useState} from "react";
-import { validateEmail } from '../../utils/helpers';
+import React from "react";
 
-function Contact() {
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-  const handleSubmit = (e) => {
-      e.preventDefault();
-      if (!errorMessage) {
-          if (!errorMessage) {
-              setFormState({ ...formState, [e.target.name]: e.target.value });
-          }
-      }
-  };
-  const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
-  const handleChange = (e) => {
-      if (e.target.name === 'email') {
-          const isValid = validateEmail(e.target.value);
-          if (!isValid) {
-              setErrorMessage('Your email is invalid.');
-          } else {
-              setErrorMessage('');
-          }
-      } else {
-          if (!e.target.value.length) {
-              setErrorMessage(`${e.target.name} is required.`);
-          } else {
-              setErrorMessage('');
-          }
-      }
-  };
-
-  // JSX
-  return (
-      <section className="container">
-          <h1 data-testid="h1tag">Contact me</h1>
-          <form id="contact-form" onSubmit={handleSubmit}>
-              <div>
-                  <label htmlFor="name">Name:</label>
-                  <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
-
-              </div>
-              <div>
-                  <label htmlFor="email">Email:</label>
-                  <input type="email" defaultValue={email} name="email" onBlur={handleChange} />
-              </div>
-              <div>
-                  <label htmlFor="message">Message:</label>
-                  <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
-              </div>
-              {errorMessage && (
-                  <div>
-                      <p className="error-text">{errorMessage}</p>
-                  </div>
-              )}
-              <button data-testid="button" type="submit">Submit</button>
-          </form>
-      </section>
-  );
-}
+const Contact = () => (
+    <div className="container">
+        <h1>Contact</h1>
+        <br></br>
+        <div className="flex-col">
+                <h2>Feel free to reach out to me. Details are below</h2>
+                <br></br>
+                <address> Email: <a href="mailto:efpramos3@gmail.com">Eder F Ramos </a></address>
+                <p>CITY: Los Angeles, CA</p>
+        </div>
+    </div>
+);
 
 export default Contact;
