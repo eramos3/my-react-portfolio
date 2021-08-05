760adbc181a9fcb136020cddbe9015c6c150d301
eramos3 <cheddarshreddar3@gmail.com>
2021-08-04 17:43:15 -0700

    updated links

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -13,42 +13,50 @@ const Portfolio = () => {
     {
       href: "https://meural.herokuapp.com/",
       name: "Meural",
-      image: Merual
+      image: Merual,
+      git:"https://github.com/stepheff1994/second-group-project"
     },
     {
       href: "https://arcane-brushlands-36937.herokuapp.com/",
       name: "Book Search Engine",
-      image: Book
+      image: Book,
+      git:"https://github.com/eramos3/book-search-engine"
     },
     {
       href: "https://eramos3.github.io/Weather-Dashboard/",
       name: "Weather Dashboard",
-      image: Weather
+      image: Weather,
+      git:"https://github.com/eramos3/Weather-Dashboard"
     },
     {
       href: "https://eramos3.github.io/work-day-scheduler/",
       name: "Day Scheduler",
-      image: Scheduler
+      image: Scheduler,
+      git:"https://github.com/eramos3/work-day-scheduler"
     },
     {
       href: "https://bdget-traker.herokuapp.com/",
       name: "Budget Tracker",
-      image: Budget
+      image: Budget,
+      git:"https://github.com/eramos3/Budget-Tracker"
     },
     {
       href: "http://la-locals.herokuapp.com/",
       name: "LA-LOCALS",
-      image: Locals
+      image: Locals,
+      git:"https://github.com/stepheff1994/la-locals-project"
     },
     {
       href: "https://rocky-depths-37921.herokuapp.com/",
       name: "Pizza Hunt",
-      image: Pizza
+      image: Pizza,
+      git:"https://github.com/eramos3/pizza-hunt"
     },
     {
       href: "https://ancient-castle-37434.herokuapp.com/",
       name: "Deep Thoughts",
-      image: Thoughts
+      image: Thoughts,
+      git:"https://github.com/eramos3/deep-thoughts"
     }
   ]);
   return (
@@ -56,9 +64,12 @@ const Portfolio = () => {
       <h1>My Portfolio</h1>
       <div className="grid-container">
         {links.map(link => (
-          <div key={link.name} className="grid-item" style={{backgroundImage: `url(${link.image})`, backgroundSize: 'cover', backgroundPosition:'center', backgroundRepeat: 'no-repeat'}}>
-            <div  className="w3-col l6"><a rel="noreferrer" target="_blank"
-              href={link.href}>{link.name}</a>
+          <div key={link.name} className="w3-card-4" >
+            <img src={link.image} alt={link.name}></img>
+            <div class="w3-container w3-center" style={{ backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
+              <p><a rel="noreferrer" target="_blank"
+                href={link.href}>{link.name}</a> / <a rel="noreferrer" target="_blank"
+                  href={link.git}>Github Page</a></p>
             </div>
           </div>
         ))}
